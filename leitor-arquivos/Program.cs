bd449530e4e46230fe672d6aca5b6a71784ed186
ycaro.batalha <ycaro.batalha@sotfplan.com.br>
2022-08-19 17:03:46 -0300

    Aula sobre funções recursivas

---
+++ leitor-arquivos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace leitor_arquivos
+{
+    class Program
+    {
+        public static void LerArquivo(int numeroArquivo)
+        {
+            string arquivoComCaminho = $@"C:\Estudos\Funcoes-Recursivas\arquivos\arq{numeroArquivo}.txt";
+            if (File.Exists(arquivoComCaminho))
+            {
+                using (StreamReader arquivo = File.OpenText(arquivoComCaminho))
+                {
+                    string linha;
+                    while ((linha = arquivo.ReadLine()) != null)
+                    {
+                        Console.WriteLine(linha);
+                    }
+                }
+            }
+            
+            var arquivoComCaminho2 = $@"C:\Estudos\Funcoes-Recursivas\arquivos\arq{numeroArquivo + 1}.txt";
+            
+            if (File.Exists(arquivoComCaminho2))
+                LerArquivo(numeroArquivo +1);
+        }
+        
+        static void Main(string[] args)
+        {
+            LerArquivo(1);
+        }
+    }
+}
